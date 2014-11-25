5c4ae0f20dbf3a9c2bd1561196936ee112bde9bb
Ryan Clark <ryan.clark.j@gmail.com>
2014-11-25 09:51:59 -0700

    make requireable, check creds

--- bin/s3signed.js
+++ bin/s3signed.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-var url = require('url');
+var s3signed = require('..');
 
 var args = process.argv.slice(2);
 if (args.length < 1) {
@@ -10,18 +8,7 @@ if (args.length < 1) {
   process.exit(1);
 }
 
-var uri = url.parse(args[0]);
-if (uri.protocol !== 's3:') {
-  console.error('Error: provide a valid S3 url');
-  process.exit(1);
-}
-var params = {
-  Bucket: uri.hostname,
-  Key: uri.pathname.slice(1),
-  Expires: Number(args[1]) || 600
-};
-
-s3.getSignedUrl('getObject', params, function(err, signedUrl) {
+s3signed(args[0], args[1], function(err, signedUrl) {
   if (err) return console.error(err);
-  process.stdout.write(signedUrl);
+  console.log(signedUrl);
 });
